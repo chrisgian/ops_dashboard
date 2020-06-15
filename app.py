82915d9c51893995ae19ecae2d3218948e4d60b5
King Chris <christopher.gian@gmail.com>
2020-06-15 00:27:21 -0700

    new app interface and update reqs

--- app.py
+++ app.py
@@ -1,186 +1,185 @@
-# -*- coding: utf-8 -*-
+"""
+This app creates a simple sidebar layout using inline style arguments and the
+dbc.Nav component.
+
+dcc.Location is used to track the current location. There are two callbacks,
+one uses the current location to render the appropriate page content, the other
+uses the current location to toggle the "active" properties of the navigation
+links.
+
+For more details on building multi-page Dash applications, check out the Dash
+documentation: https://dash.plot.ly/urls
+"""
 import dash
+import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
-import pandas as pd
 from dash.dependencies import Input, Output
 
+# Markdown text
 
-# ui
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-colors = {
-    'background': '#FFFFFFF',
-    'text': '#000000'
+#  App instantiate
+app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+# the style arguments for the sidebar. We use position:fixed and a fixed width
+SIDEBAR_STYLE = {
+    "position": "fixed",
+    "top": 0,
+    "left": 0,
+    "bottom": 0,
+    "width": "16rem",
+    "padding": "2rem 1rem",
+    "background-color": "#f8f9fa",
+}
+
+# the styles for the main content position it to the right of the sidebar and
+# add some padding.
+CONTENT_STYLE = {
+    "margin-left": "18rem",
+    "margin-right": "2rem",
+    "padding": "2rem 1rem",
 }
 
 
-# -------- data --------- #
-data_in = pd.read_csv('./data.csv')
-graph_1_data = data_in.groupby(
-    'group')[['value1', 'value2']].sum().reset_index()
+sidebar = html.Div(
+    [
+        dcc.Markdown(
+            '''
+            # Prototype App: Sidebar
+            [Dash Core Components Example: Sidebar Layout](https://dash-bootstrap-components.opensource.faculty.ai/examples/simple-sidebar/)
+            '''),
+        html.Hr(),
 
-viz1_x = graph_1_data['group'].tolist()
-viz1_y1 = graph_1_data['value1'].tolist()
-viz1_y2 = graph_1_data['value2'].tolist()
+        dbc.Nav(
+            [
+                html.Div(['Explore']),
+                dbc.NavLink("View Existing", href="/page-5", id="page-5-link"),
+                html.Div(['Data Management']),
+                dbc.NavLink("Create New", href="/page-1", id="page-1-link"),
+                dbc.NavLink("Read New", href="/page-2", id="page-2-link"),
+                dbc.NavLink("Update Existing",
+                            href="/page-3", id="page-3-link"),
+                dbc.NavLink("Delete Existing",
+                            href="/page-4", id="page-4-link")
 
-data = [
-    {'x': viz1_x, 'y': viz1_y1, 'type': 'bar', 'name': 'SF'},
-    {'x': viz1_x, 'y': viz1_y2, 'type': 'bar', 'name': 'm'}
-]
-
-
-markdown_text = '''
-# Dash and Markdown
-
-Dash apps can be written in Markdown.
-Dash uses the [CommonMark](http://commonmark.org/)
-specification of Markdown.
-Check out their [60 Second Markdown Tutorial](http://commonmark.org/help/)
-if this is your first introduction to Markdown!
-'''
-
-
-# -------- functions --------- #
-
-
-def generate_table(dataframe, max_rows=10):
-    """
-    take dataframe make data table
-    """
-    return html.Table([
-        html.Thead(
-            html.Tr([html.Th(col) for col in dataframe.columns])
+            ],
+            vertical=True,
+            pills=True,
         ),
-        html.Tbody([
-            html.Tr([
-                html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
-            ]) for i in range(min(len(dataframe), max_rows))
-        ])
-    ])
+    ],
+    style=SIDEBAR_STYLE,
+)
+
+content = html.Div(id="page-content", style=CONTENT_STYLE)
+
+app.layout = html.Div([dcc.Location(id="url"), sidebar, content])
 
 
-#
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
-
-app.layout = html.Div(
-    style={'backgroundColor': colors['background']},
-    children=[
-        html.H1(
-            children='Hello Dash',
-            style={
-                'textAlign': 'center',
-                'color': colors['text']
-            }
-        ),
-
-        html.Div(children='Dash: A web application framework for Python.',
-                 style={
-                     'textAlign': 'center',
-                     'color': colors['text']
-                 }),
-
-
-        html.Div(
-            children=[
-                generate_table(data_in.reset_index()),
-                dcc.Markdown(children=markdown_text)],
-            style={'align': 'center'}
-        ),
-        html.Div([
-
-            # column 1
-            html.Div([
-                dcc.Graph(
-                    id='example-graph-2',
-                    figure={
-                        'data': data,
-                        'layout': {
-                            'plot_bgcolor': colors['background'],
-                            'paper_bgcolor': colors['background'],
-                            'font': {
-                                'color': colors['text']
-                            }
-                        }
-                    },
-                    style={
-                        'textAlign': 'center',
-                        'color': colors['text']
-                    }
-                )
-
-            ]),
-
-            # column 2
-            html.Div([
-                html.Label('Multi-Select Dropdown'),
-                dcc.Dropdown(
-                    id='group-selector',
-                    options=[
-                        {'label': 'a', 'value': 'a'},
-                        {'label': 'b', 'value': 'b'},
-                        {'label': 'C', 'value': 'c'}
-                    ],
-                    value=['b'],
-                    multi=True
-                ),
-                dcc.Graph(id='graph-with-slider')
-            ])
-
-            
-
-
-
-        ], style={'columnCount': 2})
-
-
-
-    ])
-
-
+# this callback uses the current pathname to set the active state of the
+# corresponding nav link to true, allowing users to tell see page they are on
 @app.callback(
-    Output('graph-with-slider', 'figure'),
-    [Input('group-selector', 'value')])
-def update_figure(selected_group):
-
-    print(selected_group)
-
-    if len(selected_group) == 0:
-        filtered_df = data_in
-    else:
-        filtered_df = data_in[data_in.group.isin(selected_group)]
-
-    traces = []
-
-    # color
-    for i in filtered_df.group.unique():
-        df_by_group = filtered_df[filtered_df['group'] == i]
-        traces.append(dict(
-            x=df_by_group['value2'],
-            y=df_by_group['value1'],
-            text=df_by_group['group'],
-            mode='markers',
-            opacity=0.7,
-            marker={
-                'size': 15,
-                'line': {'width': 0.5, 'color': 'white'}
-            },
-            name=i
-        ))
-
-    return {
-        'data': traces,
-        'layout': dict(
-            xaxis={'title': 'Value 2',
-                   'range': [1, 100]},
-            yaxis={'title': 'Value 1', 'range': [1, 100]},
-            legend={'x': 0, 'y': 1},
-            hovermode='closest',
-            transition={'duration': 500},
-        )
-    }
+    [Output(f"page-{i}-link", "active") for i in range(1, 4)],
+    [Input("url", "pathname")],
+)
+def toggle_active_links(pathname):
+    if pathname == "/":
+        # Treat page 1 as the homepage / index
+        return True, False, False
+    return [pathname == f"/page-{i}" for i in range(1, 4)]
 
 
-if __name__ == '__main__':
-    app.run_server(debug=True)
+# ------ VIEW: PAGE ------- #
+page_view = html.Div(
+    [
+        html.H1('Explore Existing Data'),
+        html.P("Oh cool, this is page 4!"),
+        dbc.Row(dbc.Col(html.Div("A single column"))),
+
+        dbc.Row(
+            [
+                dbc.Col(dcc.Slider()),
+                dbc.Col(
+
+                    dcc.Dropdown(
+                        options=[
+                            {'label': 'New York City', 'value': 'NYC'},
+                            {'label': 'Montreal', 'value': 'MTL'},
+                            {'label': 'San Francisco', 'value': 'SF'}
+                        ],
+                        value=['MTL', 'NYC'],
+                        multi=False
+                    )
+
+                ),
+                dbc.Col(dcc.Dropdown(
+                    options=[
+                        {'label': 'New York City', 'value': 'NYC'},
+                        {'label': 'Montreal', 'value': 'MTL'},
+                        {'label': 'San Francisco', 'value': 'SF'}
+                    ],
+                    value=['MTL', 'NYC'],
+                    multi=True
+                )),
+            ]
+        ),
+
+    ]
+)
+
+
+# ------ View: Create ------ #
+page_create = html.Div([
+    dcc.Upload(
+        id='upload-data',
+        children=html.Div([
+            'Drag and Drop or ',
+            html.A('Select Files')
+        ]),
+        style={
+            'width': '100%',
+            'height': '60px',
+            'lineHeight': '60px',
+            'borderWidth': '1px',
+            'borderStyle': 'dashed',
+            'borderRadius': '5px',
+            'textAlign': 'center',
+            'margin': '10px'
+        },
+        # Allow multiple files to be uploaded
+        multiple=True
+    ),
+    html.Div(id='output-data-upload'),
+])
+
+
+# ------ VIEW: OTHER ------- #
+page_delete = html.P("Oh cool, this is page 4!")
+page_update = html.P("Oh cool, this is page 3!")
+
+
+
+@app.callback(Output("page-content", "children"), [Input("url", "pathname")])
+def render_page_content(pathname):
+    if pathname in ["/", "/page-1"]:
+        return page_create
+    elif pathname == "/page-2":
+        return html.P("This is the content of page 2")
+    elif pathname == "/page-3":
+        return page_update
+    elif pathname == "/page-4":
+        return page_delete
+    elif pathname == "/page-5":
+        return page_view
+    # If the user tries to reach a different page, return a 404 message
+    return dbc.Jumbotron(
+        [
+            html.H1("404: Not found", className="text-danger"),
+            html.Hr(),
+            html.P(f"The pathname {pathname} was not recognised..."),
+        ]
+    )
+
+
+if __name__ == "__main__":
+    app.run_server(port=8888, debug=True)
