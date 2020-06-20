446a66332231ba09e1f0a778a0b6ddbf9d29db8e
King Chris <christopher.gian@gmail.com>
2020-06-20 14:06:24 -0700

    break out app.py into different pages and tools

---
+++ layout.py
@@ -0,0 +1,111 @@
+import dash_bootstrap_components as dbc
+import dash_core_components as dcc
+import dash_html_components as html
+from styles import *
+
+# -- UI --#
+url = dcc.Location(id="url")
+
+# ------ VIEW: SIDEBAR ------- #
+
+page_sidebar = html.Div(
+    [
+        dcc.Markdown(
+            '''
+            # Prototype App: Sidebar
+            [Dash Core Components Example: Sidebar Layout](https://dash-bootstrap-components.opensource.faculty.ai/examples/simple-sidebar/)
+            '''),
+        html.Hr(),
+        dbc.Nav(
+            [
+                html.Div(['Explore']),
+                dbc.NavLink("View Existing", href="/page-5", id="page-5-link"),
+                html.Div(['Data Management']),
+                dbc.NavLink("Create New", href="/page-1", id="page-1-link"),
+                dbc.NavLink("Read New", href="/page-2", id="page-2-link"),
+                dbc.NavLink("Update Existing",
+                            href="/page-3", id="page-3-link"),
+                dbc.NavLink("Delete Existing",
+                            href="/page-4", id="page-4-link")
+
+            ],
+            vertical=True,
+            pills=True,
+        ),
+    ],
+    style=SIDEBAR_STYLE,
+)
+
+# ------ VIEW: Explore ------- #
+page_view = html.Div(
+    [
+        html.H1('Explore Existing Data'),
+        html.P("Oh cool, this is page 5!"),
+        dbc.Row(dbc.Col(html.Div("A single column"))),
+
+        dbc.Row(
+            [
+                dbc.Col(dcc.Slider()),
+                dbc.Col(
+
+                    dcc.Dropdown(
+                        options=[
+                            {'label': 'New York City', 'value': 'NYC'},
+                            {'label': 'Montreal', 'value': 'MTL'},
+                            {'label': 'San Francisco', 'value': 'SF'}
+                        ],
+                        value=['MTL', 'NYC'],
+                        multi=False
+                    )
+
+                ),
+                dbc.Col(dcc.Dropdown(
+                    options=[
+                        {'label': 'New York City', 'value': 'NYC'},
+                        {'label': 'Montreal', 'value': 'MTL'},
+                        {'label': 'San Francisco', 'value': 'SF'}
+                    ],
+                    value=['MTL', 'NYC'],
+                    multi=True
+                )),
+            ]
+        ),
+
+    ]
+)
+
+# ------ VIEW: OTHER ------- #
+page_delete = html.P("Oh cool, this is page 4!")
+
+# ------ VIEW: EDIT ------- #
+page_update = html.P("Oh cool, this is page 3!")
+
+# ------ VIEW: Create ------ #
+page_create = html.Div(
+    [
+        html.H1("File Browser"),
+        html.H2("Upload"),
+        dcc.Upload(
+            id="upload-data",
+            children=html.Div(
+                ["Drag and drop or click to select a file to upload."]
+            ),
+            style={
+                "width": "100%",
+                "height": "60px",
+                "lineHeight": "60px",
+                "borderWidth": "1px",
+                "borderStyle": "dashed",
+                "borderRadius": "5px",
+                "textAlign": "center",
+                "margin": "10px",
+            },
+            multiple=True,
+        ),
+        html.H2("File List"),
+        html.Ul(id="file-list"),
+    ],
+    style={"max-width": "500px"},
+)
+
+content = html.Div(id="page-content", style=CONTENT_STYLE)
---
+++ tools.py
@@ -0,0 +1,28 @@
+import base64
+import dash_core_components as dcc
+import dash_html_components as html
+import os
+from urllib.parse import quote as urlquote
+
+UPLOAD_DIRECTORY = "uploaded"
+
+def uploaded_files():
+    """List the files in the upload directory."""
+    files = []
+    for filename in os.listdir(UPLOAD_DIRECTORY):
+        path = os.path.join(UPLOAD_DIRECTORY, filename)
+        if os.path.isfile(path):
+            files.append(filename)
+    return files
+
+def file_download_link(filename):
+    """Create a Plotly Dash 'A' element that downloads a file from the app."""
+    location = "/download/{}".format(urlquote(filename))
+    return html.A(filename, href=location)
+
+
+def save_file(name, content):
+    """Decode and store a file uploaded with Plotly Dash."""
+    data = content.encode("utf8").split(b";base64,")[1]
+    with open(os.path.join(UPLOAD_DIRECTORY, name), "wb") as fp:
+        fp.write(base64.decodebytes(data))
