3903e27e944986aa2cc07c37469dd84de3ab3f79
King Chris <christopher.gian@gmail.com>
2020-06-23 21:51:16 -0700

    clean up project folder

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import dash_bootstrap_components as dbc
 UPLOAD_DIRECTORY = "uploaded"
 
 server = Flask(__name__)
+
 app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP], server=server)
 
 
@@ -13,3 +14,5 @@ app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP], server=server)
 def download(path):
     """Serve a file from the upload directory."""
     return send_from_directory(UPLOAD_DIRECTORY, path, as_attachment=True)
+
+
