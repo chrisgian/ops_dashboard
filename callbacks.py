edcdf039dea31771a90fa6db9af93314573e980c
King Chris <christopher.gian@gmail.com>
2020-06-25 21:30:43 -0700

    fix read pandas

--- callbacks.py
+++ callbacks.py
@@ -105,11 +105,7 @@ def load_selected_table(n_clicks_load, n_clicks_reset, input_value):
     if input_value is not None and context == 'button_file_load.n_clicks':
         try:
             df_loaded = pd.read_csv(
-<<<<<<< HEAD
-                os.path.join(UPLOAD_DIRECTORY, input_value)
-=======
-                "./{}/{}".format(UPLOAD_DIRECTORY, input_value))
->>>>>>> 1b5d42749affa0d429484d11e9a349ff9ff1272e
+                os.path.join(UPLOAD_DIRECTORY, input_value))
             return [
                 DataTable(
                     id='table',
